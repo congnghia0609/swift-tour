90e3b4de2f5b5dd421f73f5c7c836a2147686983
nghiatc <congnghia0609@gmail.com>
2020-02-17 16:33:16 +0700

    update code

--- Sources/swift-tour/main.swift
+++ Sources/swift-tour/main.swift
@@ -394,10 +394,97 @@ let sideLength = optionalSquare?.sideLength
 // Optional
 //print(sideLength)
 // Optional force-unwrap
-print(sideLength!)
+//print(sideLength!)
+//2.5
 
 
 // 5. Enumerations and Structures
+//== enum
+// By default, Swift assigns the raw values starting at zero and incrementing by one each time.
+// 'ace' is explicitly given a raw value of 1, and the rest of the raw values are assigned in order.
+enum Rank: Int {
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
+
+    func simpleDescription() -> String {
+        switch self {
+        case .ace:
+            return "ace"
+        case .jack:
+            return "jack"
+        case .queen:
+            return "queen"
+        case .king:
+            return "king"
+        default:
+            return String(self.rawValue)
+        }
+    }
+}
+let ace = Rank.ace
+//print(ace)
+//ace
+let aceRawValue = ace.rawValue
+//print(aceRawValue)
+//1
+if let convertedRank = Rank(rawValue: 3) {
+    let threeDesciption = convertedRank.simpleDescription()
+    print(threeDesciption)
+    //3
+}
+
+enum Suit {
+    case spades, hearts, diamonds, clubs
+
+    func simpleDescription() -> String {
+        switch self {
+        case .spades:
+            return "spades"
+        case .hearts:
+            return "hearts"
+        case .diamonds:
+            return "diamonds"
+        case .clubs:
+            return "clubs"
+        }
+    }
+}
+let hearts = Suit.hearts
+let heartsDesciption = hearts.simpleDescription()
+//print(heartsDesciption)
+
+enum ServerResponse {
+    case result(String, String)
+    case failure(String)
+}
+let success = ServerResponse.result("6:00 am", "8:09 pm")
+let failure = ServerResponse.failure("Out og cheese")
+switch success {
+case let .result(sunrise, sunset):
+    print("Sunrise is at \(sunrise) and sunset is at \(sunset).")
+case let .failure(message):
+    print("Failure... \(message)")
+}
+//Sunrise is at 6:00 am and sunset is at 8:09 pm.
+
+
+//== struct
+struct Card {
+    var rank: Rank
+    var suit: Suit
+    func simpleDescription() -> String {
+        return "The \(rank.simpleDescription()) of \(suit.simpleDescription())"
+    }
+}
+let threeOfSpades = Card(rank: .three, suit: .spades)
+let threeOfSpadesDescription = threeOfSpades.simpleDescription()
+//print(threeOfSpadesDescription)
+//The 3 of spades
+
+
+
+
 
 
 
