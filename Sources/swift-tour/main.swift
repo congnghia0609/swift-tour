792b6aef902d711dcebfe08f3d4a42fe62ea6416
nghiatc <congnghia0609@gmail.com>
2020-02-17 15:54:44 +0700

    update code

--- Sources/swift-tour/main.swift
+++ Sources/swift-tour/main.swift
@@ -98,6 +98,14 @@ if let name = optionalName {
 // print(greeting)
 // Hello, guest
 
+var nn:String? = nil //"nghia"
+//let mm = nn! + " tran" //=> Fatal error: Unexpectedly found nil while unwrapping an Optional value.
+//print(mm)
+if nn != nil {
+    let mm = nn! + " tran"
+    print(mm)
+}
+
 // to provide a default value using the ?? operator.
 let nickName: String? = nil
 let fullName: String = "John Appleseed"
@@ -267,6 +275,133 @@ let mappedNumbers = numbers.map({number in 3*number})
 let sortedNumbers = numbers.sorted{$0 > $1}
 //print(sortedNumbers)
 //[20, 19, 12, 7]
+
+
+// 4. Objects and Classes
+//== class
+class Shape {
+    var numberOfSides = 0
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+var shape = Shape()
+shape.numberOfSides = 7
+var shapeDescription = shape.simpleDescription()
+//print(shapeDescription)
+//A shape with 7 sides.
+
+class NamedShape {
+    var numverOfSides: Int = 0
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+
+    func simpleDescription() -> String {
+        return "A shape with \(numverOfSides) sides."
+    }
+}
+
+class Square: NamedShape {
+    var sideLength: Double
+
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numverOfSides = 4
+    }
+
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)."
+    }
+}
+
+let test = Square(sideLength: 5.2, name: "my test square")
+var area = test.area()
+//print(area)
+//27.040000000000003
+var sdesc = test.simpleDescription()
+//print(sdesc)
+//A square with sides of length 5.2.
+
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numverOfSides = 3
+    }
+
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)."
+    }
+}
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+//print(triangle.perimeter)
+//9.3
+triangle.perimeter = 9.9
+//print(triangle.perimeter)
+//9.9
+//print(triangle.sideLength)
+//3.3000000000000003
+
+// code run before and after setting a new value, use 'willSet' and 'didSet'.
+class TriangleAndSquare {
+    var triangle: EquilateralTriangle {
+        willSet {
+            square.sideLength = newValue.sideLength
+        }
+    }
+    var square: Square {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    init(size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+
+var triangleAndSquare = TriangleAndSquare(size: 10, name: "anothe test shape")
+//print(triangleAndSquare.square.sideLength)
+//10.0
+//print(triangleAndSquare.triangle.sideLength)
+//10.0
+triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+//print(triangleAndSquare.triangle.sideLength)
+//50.0
+
+let optionalSquare: Square? = Square(sideLength: 2.5, name: "optional square")
+let sideLength = optionalSquare?.sideLength
+// Optional
+//print(sideLength)
+// Optional force-unwrap
+print(sideLength!)
+
+
+// 5. Enumerations and Structures
+
+
+
+
 
 
 
