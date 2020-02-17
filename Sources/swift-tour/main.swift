e7da60b751ef9ac4d2a643d355b29c16187a2b2d
nghiatc <congnghia0609@gmail.com>
2020-02-17 16:58:53 +0700

    update code

--- Sources/swift-tour/main.swift
+++ Sources/swift-tour/main.swift
@@ -483,13 +483,65 @@ let threeOfSpadesDescription = threeOfSpades.simpleDescription()
 //The 3 of spades
 
 
+// 6. Protocols and Extensions
+//== protocol
+// Use protocol to declare a protocol.
+protocol ExampleProtocol {
+    var simpleDescription: String { get }
+    // mutating: to mark a method that modifies the structure.
+    mutating func adjust()
+}
+
+class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "A very simple class."
+    var anotherProperty: Int = 69105
+
+    //doesn’t need any of its methods marked as 'mutating'
+    // because methods on a class can always modify the class.
+    func adjust() {
+        simpleDescription += " Now 100% adjusted."
+    }
+}
+var a = SimpleClass()
+a.adjust()
+let aDescription = a.simpleDescription
+//print(aDescription)
+//A very simple class. Now 100% adjusted.
+
+struct SimpleStructure: ExampleProtocol {
+    var simpleDescription: String = "A simple structure"
+
+    mutating func adjust() {
+        simpleDescription += " (adjusted)"
+    }
+}
+var b = SimpleStructure()
+b.adjust()
+let bDescription = b.simpleDescription
+//print(bDescription)
+//A simple structure (adjusted)
 
 
+//== extension
+//Use extension to add functionality to an existing type,
+// such as new methods and computed properties.
+extension Int: ExampleProtocol {
+    var simpleDescription: String {
+        return "The number \(self)"
+    }
 
+    mutating func adjust() {
+        self += 42
+    }
+}
+//print(7.simpleDescription)
+//The number 7
 
-
-
-
+let protocolValue: ExampleProtocol = a
+//print(protocolValue.simpleDescription)
+//A very simple class. Now 100% adjusted.
+//print(protocolValue.anotherProperty)
+//=> error: value of type 'ExampleProtocol' has no member 'anotherProperty'
 
 
 
